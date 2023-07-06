3c201ad1b437059c48480f2d04eb0ceef9882949
Guru2308 <chatheriyan2308@gmail.com>
2023-07-06 21:38:58 +0530

    s&f

---
+++ Basics/struct_and_func.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+struct Rect{
+    int length;
+    int breadth;
+}r ;
+
+void init(struct Rect *r, int l, int b){
+    r->length = l;
+    r->breadth = b;
+}
+
+int area(struct Rect *r){
+    return r->length*r->breadth;
+}
+
+int changeL(struct Rect *r, int l){
+    return r->length = l;
+}
+
+int main(){
+    struct Rect r;
+    init(&r,10,5);
+    cout<<area(&r)<<endl;
+    changeL(&r, 20);
+    cout<<area(&r)<<endl;
+    
+    return 0;
+}
